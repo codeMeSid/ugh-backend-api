a63b2bf30b26a55d4144f27625a250f0c3cd236a
codeMeSid <siddhanthbajoria@gmail.com>
2022-10-20 17:34:09 +0530

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git pull
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker image  $(docker image ls -q)
+docker container stop $(docker container ls -a -q)
+docker container rm $(docker container ls -a -q)
+docker image rm  $(docker image ls -a -q)
 docker build -t siddy/ugh-api:latest .
 docker run -d -p 4000:4000 siddy/ugh-api:latest
