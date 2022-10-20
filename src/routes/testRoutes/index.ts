e98ee49034d20d401f2e2d502c32e30a15597c36
codeMeSid <siddhanthbajoria@gmail.com>
2022-10-20 16:15:46 +0530

    test-api added

--- src/routes/testRoutes/index.ts
+++ src/routes/testRoutes/index.ts
@@ -1,3 +1,16 @@
 import { ApiRoute } from "../../types/ApiRoute";
 
-export const testRoutes: Array<ApiRoute> = [];
+export const testRoutes: Array<ApiRoute> = [
+  {
+    url: "health-check",
+    method: "get",
+    middlewares: [],
+    controller: (req, res) => {
+      res.json({ msg: "Healthy Api" });
+    },
+    doc: {
+      responseStructure: { msg: "Healthy" },
+      text: "Just a api health check api",
+    },
+  },
+];
